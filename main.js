46c98702cb70f5eff7f41cc97ea0bc238fc9af81
Astha Pant <58358546+asthapant@users.noreply.github.com>
2021-07-07 01:11:51 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,26 @@
-let Peer = require('simple-peer')
-
+let Peer = require('simple-peer')   // importing simple peer package
 console.log(filter)
-const videoGrid = document.getElementById('video-grid')
+const videoGrid = document.getElementById('video-grid')   
 const videoGrid2 = document.getElementById('video-grid2')
 
-let socket = io('/')
-var video = document.querySelector('video')
-const inviteButton = document.querySelector("#inviteButton");
+let socket = io('/')                         //initialising socket
+var video = document.querySelector('video') //reference to our own video
+const inviteButton = document.querySelector("#inviteButton"); 
 
-let client = {}
+let client = {}  //define the client
 
-navigator.mediaDevices.getUserMedia({ video: true, audio: true })
-    .then(stream => {
-        socket.emit('NewClient')
-        video.srcObject = stream
-        
+navigator.mediaDevices.getUserMedia({ video: true, audio: true })  //getting user's stream through the browser
+    .then(stream => {                       //if user gives the permission, proceed
+        socket.emit('NewClient')            //message in the backend about the new client
+        video.srcObject = stream           //display our own video
         video.play()
         mystream = stream;
 
-        function InitPeer(type) {
-            let peer = new Peer({ initiator: (type == 'init') ? true : false, stream: stream, trickle: false })
-            peer.on('stream', function (stream) {
-                CreateVideo(stream)
-                const video2=document.getElementById("peerVideo")
+        function InitPeer(type) {         //initialise a new peer
+            let peer = new Peer({ initiator: (type == 'init') ? true : false, stream: stream, trickle: false }) //if type is true, send the offer to stream
+            peer.on('stream', function (stream) {        
+                CreateVideo(stream)                                       //create a new video when we get stream from other user
+                const video2=document.getElementById("peerVideo")         //reference to the other person's video
                 videoGrid2.append(video2)
                 
             })
@@ -35,16 +33,16 @@ navigator.mediaDevices.getUserMedia({ video: true, audio: true })
           return peer
       
 
-        peer.on('close', function () {
-                document.getElementById("peerVideo").remove();
+        peer.on('close', function () {                              //destroy the video when peer is closed
+                document.getElementById("peerVideo").remove();   
                 peer.destroy()
              })
             return peer
         }
-        function MakePeer() {
-            client.gotAnswer = false
+        function MakePeer() {                                     //for peer of type init                                                                      
+            client.gotAnswer = false                              
             let peer = InitPeer('init')
-            peer.on('signal', function (data) {
+            peer.on('signal', function (data) {                   
                 if (!client.gotAnswer) {
                     socket.emit('Offer', data)
                 }
@@ -52,7 +50,7 @@ navigator.mediaDevices.getUserMedia({ video: true, audio: true })
             client.peer = peer
         }
 
-        function FrontAnswer(offer) {
+        function FrontAnswer(offer) {                           //for peer of type not init
             let peer = InitPeer('notInit')
             peer.on('signal', (data) => {
                 socket.emit('Answer', data)
@@ -61,12 +59,12 @@ navigator.mediaDevices.getUserMedia({ video: true, audio: true })
             client.peer = peer
         }
 
-        function SignalAnswer(answer) {
+        function SignalAnswer(answer) {                    
             client.gotAnswer = true
             let peer = client.peer
             peer.signal(answer)
         }
-        function CreateVideo(stream) {
+        function CreateVideo(stream) {                        //create and append the video             
       
             let video = document.createElement('video')
             video.id = 'peerVideo'
@@ -87,7 +85,7 @@ navigator.mediaDevices.getUserMedia({ video: true, audio: true })
             })
 
         }
-        function SessionActive() {
+        function SessionActive() {                      //someone else opens the url when two people are already in a call
             document.write('Session Active with 2 users. Please come back later!')
         }
      
@@ -99,20 +97,20 @@ navigator.mediaDevices.getUserMedia({ video: true, audio: true })
      videoGrid.append(video);
   })
 
-    function show(){
+    function show(){                               //runs when invite button is clicked
       prompt(
         "Copy this link and send it to someone you want to video call with :)",
         window.location.href
       );
     };
  
-    function info(){
+    function info(){                            //runs when about button is clicked
       alert(
         "Hey there! This is a video conferencing platform designed to facilitate video call between two users. It allows you to: \n\u2022Mute/Unmute your audio\n\u2022Show/Stop your video\n\u2022Invite Someone"
       );
     };
    
-    function filt(){
+    function filt(){                          //adds filter as per selection
 
       var video = document.querySelector('video')
       const filter = document.getElementById("filters")
@@ -123,7 +121,7 @@ navigator.mediaDevices.getUserMedia({ video: true, audio: true })
   
   }
   
-    function setMuteButton(){
+    function setMuteButton(){                  
         const html = `
           <i class="fas fa-microphone"></i>
           <span>Mute</span>
@@ -139,7 +137,7 @@ navigator.mediaDevices.getUserMedia({ video: true, audio: true })
         document.querySelector('.main__mute_button').innerHTML = html;
       }
       
-      function setStopVideo(){
+      function setStopVideo(){                    
         const html = `
           <i class="fas fa-video"></i>
           <span>Stop Video</span>
@@ -155,7 +153,7 @@ navigator.mediaDevices.getUserMedia({ video: true, audio: true })
         document.querySelector('.main__video_button').innerHTML = html;
       }
     
-function muteUnmute(){
+function muteUnmute(){                                             //mute and unmute functions when mute button is clicked                               
         
         const enabled = mystream.getAudioTracks()[0].enabled;
         if (enabled) {
@@ -168,7 +166,7 @@ function muteUnmute(){
       }
     
       
-    function playStop(){
+    function playStop(){                                     //show and stop video functions when video button is clicked
         console.log('object')
         let enabled = mystream.getVideoTracks()[0].enabled;
         if (enabled) {
