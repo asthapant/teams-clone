0450f0d07641d7215f95391f7bcfaa315a811c19
Astha Pant <58358546+asthapant@users.noreply.github.com>
2021-07-07 01:18:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
-const express = require('express')
+const express = require('express')                                //modules and dependencies
 const app = express()
-const http = require('http').Server(app)
-const io = require('socket.io')(http)
-const port = process.env.PORT || 3000
+const http = require('http').Server(app)                         //supply app to the http server
+const io = require('socket.io')(http)                            //passing http to socket.io
+const port = process.env.PORT || 3000                            //setting to any port available or 3000 port
 
-app.use(express.static(__dirname + "/public"))
-let clients = 0
+app.use(express.static(__dirname + "/public"))                  //static files to load 
+let clients = 0                                                   //no clients initially
 
-io.on('connection', function (socket) {
+io.on('connection', function (socket) {                         //run when the connection between front and back end is made            
     socket.on("NewClient", function () {
-        if (clients < 2) {
+        if (clients < 2) {                                     //check if there is one client then create peer
             if (clients == 1) {
                 this.emit('CreatePeer')
             }
         }
         else
-            this.emit('SessionActive')
+            this.emit('SessionActive')                        //else send the session active message
         clients++;
     })
    
-    socket.on('Offer', SendOffer)
-    socket.on('Answer', SendAnswer)
+    socket.on('Offer', SendOffer)                            //if offer coming from frontend, send to backend
+    socket.on('Answer', SendAnswer)                         //if answer is coming, send to backend
     socket.on('disconnect', Disconnect)
 })
 
@@ -32,7 +32,7 @@ function Disconnect() {
     }
 }
 
-function SendOffer(offer) {
+function SendOffer(offer) {                              //send offer to the other user
     this.broadcast.emit("BackOffer", offer)
 }
 
